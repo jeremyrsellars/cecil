a710b25035e0ee0b2760575cbd597f45c226377f
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2017-12-21 11:18:50 -0600

    Fix github pages directory

--- scripts/commit_hint.cmd
+++ scripts/commit_hint.cmd
@@ -1,6 +1,6 @@
 @echo off
 SET GIT_DESCRIPTION=%~1
 IF "x%GIT_DESCRIPTION%"=="x" for /f "usebackq" %%x in (`git describe --long --tags --dirty`) do set GIT_DESCRIPTION=%%x
-ECHO git add "%~dp0..\doc"
+ECHO git add "%~dp0..\docs"
 ECHO git commit -m "Publish doc at %GIT_DESCRIPTION%"
 ECHO git push
--- scripts/pub_doc.cmd
+++ scripts/pub_doc.cmd
@@ -2,7 +2,7 @@
 SETLOCAL
 set root=%~dp0..
 pushd "%~dp0"
-set doc=%root%\doc
+set doc=%root%\docs
 if exist "%doc%" RD "%doc%" /q/s
 if not exist "%doc%" MD "%doc%"
 xcopy /s /y  /exclude:exclusions.txt "%root%\src\cecil\resources\public\*" "%doc%\"
