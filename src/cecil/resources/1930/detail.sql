805cba3d8f7e340398454fb3b7eb3e174193b5ea
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2017-12-26 14:34:27 -0600

    1930: obfuscate

--- src/cecil/resources/1930/detail.sql
+++ src/cecil/resources/1930/detail.sql
@@ -1,30 +1,30 @@
   select distinct
     sir.item_id,
-    ocs.catalog_cd,
-    (SELECT DISPLAY FROM CODE_VALUE WHERE CODE_VALUE = ocir.catalog_cd AND ACTIVE_IND = 1 /*uar_get_code_display(ocir.catalog_cd)*/) AS ITEM_PRIMARY,
+    ocs.cat_id,
+    (SELECT DISPLAY FROM CODE_VALUE WHERE CODE_VALUE = ocir.cat_id AND ACTIVE_IND = 1 /*uar_get_code_display(ocir.cat_id)*/) AS ITEM_PRIMARY,
     sir.synonym_id,
-    (SELECT DISPLAY FROM CODE_VALUE WHERE CODE_VALUE = ocs.catalog_cd AND ACTIVE_IND = 1 /*uar_get_code_display(ocs.catalog_cd)*/) AS SYN_PRIMARY,
+    (SELECT DISPLAY FROM CODE_VALUE WHERE CODE_VALUE = ocs.cat_id AND ACTIVE_IND = 1 /*uar_get_code_display(ocs.cat_id)*/) AS SYN_PRIMARY,
     ocs.mnemonic,
-    (SELECT DISPLAY FROM CODE_VALUE WHERE CODE_VALUE = ocs.mnemonic_type_cd AND ACTIVE_IND = 1 /*uar_get_code_display(ocs.mnemonic_type_cd)*/) AS MNEMONIC_TYPE_CD,
+    (SELECT DISPLAY FROM CODE_VALUE WHERE CODE_VALUE = ocs.mnemonic_type_id AND ACTIVE_IND = 1 /*uar_get_code_display(ocs.mnemonic_type_id)*/) AS MNEMONIC_TYPE_ID,
      mi.value AS ITEM_DESC
   from
     synonym_item_r sir
-  inner join order_catalog_item_r ocir
+  inner join order_cat_item_r ocir
     on ocir.item_id = sir.item_id
-  inner join order_catalog_synonym ocs
+  inner join order_cat_synonym ocs
     on ocs.synonym_id = sir.synonym_id
-    and ocs.catalog_cd != ocir.catalog_cd
+    and ocs.cat_id != ocir.cat_id
   left join med_identifier mi
     on mi.item_id = outerjoin(ocir.item_id)
     and mi.med_product_id = outerjoin(0)
     and mi.primary_ind = outerjoin(1)
-    and mi.med_identifier_type_cd = outerjoin(value((select CODE_VALUE from CODE_VALUE cv where cv.cki = 'CKI.CODEVALUE!3290' and ACTIVE_IND = 1 )))
+    and mi.med_identifier_type_id = outerjoin(value((select CODE_VALUE from CODE_VALUE cv where cv.cki = 'CKI.CODEVALUE!3290' and ACTIVE_IND = 1 )))
   where sir.item_id not in (
     select
       ocir.item_id
     from
-      order_catalog_item_r ocir
+      order_cat_item_r ocir
     group by ocir.item_id
-    having count(ocir.catalog_cd) > 1
+    having count(ocir.cat_id) > 1
   )
   order by sir.item_id
