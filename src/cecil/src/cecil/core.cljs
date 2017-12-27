6212eac81c8e07ce9eb443cc9e58859101fd9895
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2017-12-27 01:11:58 -0600

    Fix application in advanced compilation

--- src/cecil/src/cecil/core.cljs
+++ src/cecil/src/cecil/core.cljs
@@ -6,7 +6,8 @@
    [sablono.core :as sab :include-macros true]
    [cecil.cki :as cki]
    [cecil.util :as util]
-   [cecil.ccl-to-sql :as cts]))
+   [cecil.ccl-to-sql :as cts]
+   [react :as react]))
 
 (enable-console-print!)
 
@@ -33,7 +34,7 @@
 (defn main []
   ;; conditionally start the app based on whether the #main-app-area
   ;; node is on the page
-  (if-let [node (.getElementById js/document "main-app-area")]
+  (when-let [node (.getElementById js/document "main-app-area")]
     (.render js/ReactDOM (sab/html (app))
      node)))
 
