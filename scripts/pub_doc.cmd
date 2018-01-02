e0761d089b8ea57d09dbd2000ee22480f72eab7d
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2018-01-02 10:53:12 -0600

    index.html: Pretend js and css assets are version
    So Ctrl+F5 to completely refresh isn't necessary.

--- scripts/pub_doc.cmd
+++ scripts/pub_doc.cmd
@@ -6,4 +6,5 @@ set doc=%root%\docs
 if exist "%doc%" RD "%doc%" /q/s
 if not exist "%doc%" MD "%doc%"
 xcopy /s /y  /exclude:exclusions.txt "%root%\src\cecil\resources\public\*" "%doc%\"
+FOR %%f in (%doc%\*.html) DO powershell -f %~dp0versionize-html.ps1 %%f
 popd
